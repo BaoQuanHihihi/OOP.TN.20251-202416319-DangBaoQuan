f4a8c793943a367b4b7fc3531dec868c765de490
baoquanhihihi <baoquanbaoquan9088@gmail.com>
2025-11-16 13:54:04 +0700

    Adding static attribute to DigitalVideoDisc class

--- AimsProject/src/com/hust/kstn/test/Test.java
+++ AimsProject/src/com/hust/kstn/test/Test.java
@@ -7,15 +7,13 @@ public class Test {
     public static void main(String[] args) {
         Cart cart = new Cart();
 
-        DigitalVideoDisc dvd1 = new DigitalVideoDisc(1, "The Lion King", "Animation", "Roger Allers", 87, 19.95f);
-        DigitalVideoDisc dvd2 = new DigitalVideoDisc(2, "Star Wars", "Science Fiction", "George Lucas", 124, 24.95f);
-        DigitalVideoDisc dvd3 = new DigitalVideoDisc(3, "Aladdin", "Animation", "John Musker", 90, 18.99f);
-        DigitalVideoDisc dvd4 = new DigitalVideoDisc(4, "The Godfather", "Crime", "Francis Coppola", 175, 22.5f);
-        DigitalVideoDisc dvd5 = new DigitalVideoDisc(5, "Inception", "Action", "Christopher Nolan", 148, 21.0f);
+        DigitalVideoDisc dang = new DigitalVideoDisc("The Lion King", "Animation", "Roger Allers", 87, 19.95f);
+        DigitalVideoDisc bao = new DigitalVideoDisc("Star Wars", "Science Fiction", "George Lucas", 124, 24.95f);
+        DigitalVideoDisc quan = new DigitalVideoDisc("Aladdin", "Animation", "John Musker", 90, 18.99f);
+        DigitalVideoDisc dep = new DigitalVideoDisc("The Godfather", "Crime", "Francis Coppola", 175, 22.5f);
+        DigitalVideoDisc trai = new DigitalVideoDisc("Inception", "Action", "Christopher Nolan", 148, 21.0f);
 
-        DigitalVideoDisc[] myArray = {dvd3, dvd4, dvd5};
-
-        cart.addDVD(dvd3, dvd4, dvd1);
+        cart.addDVD(bao, quan, dep, dang);
         cart.viewShoppingCart();
     }
 }
